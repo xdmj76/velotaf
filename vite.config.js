375b870c2efc14fb73d033ab02f5c2be817a56d8
Xavier Denis <xdmj@yahoo.fr>
2026-04-06 18:42:02 +0200

    fix: Fix manifest not included

--- vite.config.js
+++ vite.config.js
@@ -20,36 +20,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Velotaf',
-        short_name: 'Velotaf',
-        description: 'Déclaration des trajets à vélo',
-        theme_color: '#10b981',
-        background_color: '#ffffff',
-        display: 'standalone',
-        shortcuts: [
-          {
-            name: "Valider aujourd'hui",
-            short_name: "Valider",
-            description: "Enregistre le trajet du jour",
-            url: "/velotaf/?action=add_today",
-            icons: [{ src: "pwa-192x192.png", sizes: "192x192" }]
-          }
-        ],
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'manifest.webmanifest'],
     })
   ]
 })
